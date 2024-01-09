af3dc9f65bce21309d20acb2bd1fc1e36baf7622
Davian Affognon <affogdav000@warren.k12.in.us>
2024-01-09 12:17:17 -0500

    sem2u1l2

--- 01_Strings/02_Basic String Operations/Assignment.py
+++ 01_Strings/02_Basic String Operations/Assignment.py
@@ -1,5 +1,4 @@
-bit_of_text = "" #YOUDO get this from user input.  
+bit_of_text = input("Enter some text: ")
 
 for char in bit_of_text:
-    #YOUDO:  print char on its own line.  You will need ord to do this.  
-    pass #YOUDO:  remove the pass when done.  
+    print(ord(char))
