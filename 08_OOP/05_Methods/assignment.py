7fa37969f8d845a1b15468eb1d6cc15591a4360c
Davian Affognon <affogdav000@warren.k12.in.us>
2024-03-14 12:04:32 -0400

    nlqajdjsaid

--- 08_OOP/05_Methods/assignment.py
+++ 08_OOP/05_Methods/assignment.py
@@ -14,18 +14,30 @@ class Rectangle:
     def get_height(self) -> float:
         return self.__height
 
-    # YOUDO the get_base method
+    def get_base(self) -> float:
+        return self.__base
 
     def get_perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
 
-    # Youdo get_area method
+    def get_area(self) -> float:
+        return self.__base * self.__height
+    
     
     def __str__(self) -> str:
         #Rectangle with base:3, height:4
         return "Rectangle with base:" + str(self.__base) + ", height:" + str(self.__height)
     
 
-
-# YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-# using only the methods not the fields/property/attributes
+rectangle1 = Rectangle(3,4)
+print(rectangle1)
+print("The base of rectangle1 is", rectangle1.get_base())
+print("The height of rectangle1 is", rectangle1.get_height())
+print("The perimeter of rectangle1 is", rectangle1.get_perimeter())
+print("The area of rectangle1 is", rectangle1.get_area())
+rectangle2 = Rectangle(4,5)
+print(rectangle2)
+print("The base of rectangle2 is", rectangle2.get_base())
+print("The height of rectangle2 is", rectangle2.get_height())
+print("The perimeter of rectangle2 is", rectangle2.get_perimeter())
+print("The area of rectangle2 is", rectangle2.get_area())
