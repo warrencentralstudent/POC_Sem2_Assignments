98bc9091c04fa212352d087a4fe424a43dff3094
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-03-21 11:56:38 -0400

    updates

--- 10_Functional Programming/03_File Handling/assignment.py
+++ 10_Functional Programming/03_File Handling/assignment.py
@@ -1,7 +1,9 @@
-file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\myFile.txt"
+file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\assignment.py"
 
 try:
     stream = open(file_path)
+    #YOUDO print to console using stream.read()
+    #YOUDO don't forget to close the stream
 except:
     pass
 ##YOUDO rest of the program for reading and printing to the console
